2f1a81f92d3333deebc24ced5a9c0566cab90e0d
fiftekhar <iftekhar.f.19@gmail.com>
2022-12-21 23:53:17 -0500

    change handlers

--- src/components/containers/EditEmployeeContainer.js
+++ src/components/containers/EditEmployeeContainer.js
@@ -4,3 +4,43 @@ import { Redirect, Link } from 'react-router-dom';
 
 import { fetchEmployeeThunk, editEmployeeThunk, fetchAllTasksThunk  } from '../../store/thunks';
 
+class EditEmployeeContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: "",
+            lastname: "",
+            department: "",
+            taskId: null,
+            redirect: false,
+            redirectId: null,
+            error: ""
+        };
+    }
+
+    componentDidMount() {
+        this.props.fetchEmployee(this.props.match.params.id);
+        this.props.fetchTasks();
+        this.setState({
+            firstname: this.props.employee.firstname, 
+            lastname: this.props.employee.lastname,
+            department: this.props.employee.department,
+            taskId: this.props.employee.taskId, 
+        });
+    }
+
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+    }
+  
+    handleSelectChange = event => {
+       
+        if (event.target.value === "not assigned to anything") {
+            this.setState({taskId:null});
+        } else {
+            this.setState({taskId: event.target.value})
+        }
+    }
+}
