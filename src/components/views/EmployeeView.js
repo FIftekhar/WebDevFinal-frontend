784848ef4d76fb653f426e5a77ee62ede99c3b3e
arahman63 <Arifur.Rahman63@myhunter.cuny.edu>
2022-12-22 01:33:38 -0500

    edited employee view and renamed files

--- src/components/views/EmployeeView.js
+++ src/components/views/EmployeeView.js
@@ -1,45 +1,63 @@
+/** @format */
+
 import { Link } from "react-router-dom";
 
-
 const EmployeeView = (props) => {
-  const {employee, editTask, allTasks} = props;
-  let assignedTasks = allTasks.filter(task => task.employeeID===employee.id);
-  let availableTasks = allTasks.filter(task => task.employeeID!==employee.id);
-  
-  return (
-    <div>      
-      <h1>{employee.firstname}</h1>
-      <h3>{employee.department}</h3>
-      <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
-        <div>Assigned tasks:
-        {assignedTasks.map( task => {
-          return (
-            <div key={task.id}>
-            <Link to={`/task/${task.id}`}>
-              <h4>{task.description}</h4>
-            </Link>
-            <button onClick={() => editTask({id:task.id, employeeID: null})}>x</button>
-            </div>
-          );
-        })}</div>
-        <div>Available tasks:
-        {availableTasks.map( task => {
-          return (
-            <div key={task.id}>
-            <Link to={`/task/${task.id}`}>
-              <h4>{task.description}</h4>
-            </Link>
-            <button onClick={() => editTask({id:task.id, employeeID: employee.id})}>+</button>
-            </div>
-          );
-        })}</div>
-
-      </div>
-
-  
-    </div>
+  const { employee, editTask, allTasks } = props;
+  let assignedTasks = allTasks.filter(
+    (task) => task.employeeId === employee.id
+  );
+  let availableTasks = allTasks.filter(
+    (task) => task.employeeId !== employee.id
   );
 
+  return (
+    <div>
+      <h1>{employee.firstname}</h1>
+      <h3>{employee.department}</h3>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}>
+        <div>
+          Assigned tasks:
+          {assignedTasks.map((task) => {
+            return (
+              <div key={task.id}>
+                <Link to={`/task/${task.id}`}>
+                  <h4>{task.title}</h4>
+                </Link>
+                <button
+                  onClick={() => editTask({ id: task.id, employeeId: null })}>
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          Available tasks:
+          {availableTasks.map((task) => {
+            return (
+              <div key={task.id}>
+                <Link to={`/task/${task.id}`}>
+                  <h4>{task.title}</h4>
+                </Link>
+                <button
+                  onClick={() =>
+                    editTask({ id: task.id, employeeId: employee.id })
+                  }>
+                  +
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeView;
