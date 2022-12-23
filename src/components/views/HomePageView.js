12f3802ec7e019e193cde8cd59261c72fc6e967c
arahman63 <Arifur.Rahman63@myhunter.cuny.edu>
2022-12-22 23:29:52 -0500

    edited task views, a new label for completion status

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,31 +1,29 @@
+/** @format */
+
 import "./HomePageView.css";
 import Heading from "../Heading";
 
-import { Link } from 'react-router-dom';
-import Stack from 'react-bootstrap/Stack';
+import { Link } from "react-router-dom";
+import Stack from "react-bootstrap/Stack";
 
 const HomePageView = () => {
-	console.log("in here ")
-	return (
-		<div>
-			<Heading/>
+  console.log("in here ");
+  return (
+    <div>
+      <Heading />
 
-			{/* row of buttons to go to different pages (Instructors and Tasks) */}
-			<Stack direction='horizontal' gap={3} id='topButtons'>
-				<Link to={'/employees'}>
-					<button>
-						All Employees
-					</button>
-				</Link>
+      {/* row of buttons to go to different pages (Employees and Tasks) */}
+      <Stack direction="horizontal" gap={3} id="topButtons">
+        <Link to={"/employees"}>
+          <button>All Employees</button>
+        </Link>
 
-				<Link to={'/tasks'}>
-					<button>
-						All Tasks
-					</button>
-				</Link>
-			</Stack>
-		</div>
-	);
-}
+        <Link to={"/tasks"}>
+          <button>All Tasks</button>
+        </Link>
+      </Stack>
+    </div>
+  );
+};
 
 export default HomePageView;
--- src/components/views/NewTaskView.js
+++ src/components/views/NewTaskView.js
@@ -1,44 +1,81 @@
+/** @format */
+
 import Heading from "../Heading";
 
 const NewTaskView = (props) => {
-	const {handleChange, handleSubmit } = props;
+  const { handleChange, handleSubmit } = props;
 
-	return (
-		<div>
-			<Heading />
-			<div className="root">
-			<div className="formContainer">
-				<div className="formTitle">
-				<h2 style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
-					New Task
-				</h2>
-				</div>
-				<form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
-				<label style= {{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
-				<input type="text" name="description" onChange ={(e) => handleChange(e)} />
-				<br/>
-				<br/>
+  return (
+    <div>
+      <Heading />
+      <div className="root">
+        <div className="formContainer">
+          <div className="formTitle">
+            <h2
+              style={{
+                fontWeight: "bold",
+                fontFamily: "Courier, sans-serif",
+                fontSize: "20px",
+                color: "#11153e",
+              }}>
+              New Task
+            </h2>
+          </div>
+          <form
+            style={{ textAlign: "center" }}
+            onSubmit={(e) => handleSubmit(e)}>
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Description:{" "}
+            </label>
+            <input
+              type="text"
+              name="description"
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <br />
 
-				<label style={{color:'#11153e', fontWeight: 'bold'}}>Priority Level: </label>
-				<input type="text" name="priority" onChange={(e) => handleChange(e)} />
-				<br/>
-				<br/>
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Priority Level:{" "}
+            </label>
+            <input
+              type="text"
+              name="priority"
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <br />
 
-				<label style={{color:'#11153e', fontWeight: 'bold'}}>Employee ID #: </label>
-				<input type="text" name="employeeID" onChange={(e) => handleChange(e)} />
-				<br/>
-				<br/>
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Completion Status:{" "}
+            </label>
+            <input
+              type="text"
+              name="completionstatus"
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <br />
 
-				<button type="submit">
-					Submit
-				</button>
-				<br/>
-				<br/>
-				</form>
-				</div>
-			</div>
-		</div>
-	)
-}
+            <label style={{ color: "#11153e", fontWeight: "bold" }}>
+              Employee ID #:{" "}
+            </label>
+            <input
+              type="text"
+              name="employeeID"
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <br />
+
+            <button type="submit">Submit</button>
+            <br />
+            <br />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NewTaskView;
