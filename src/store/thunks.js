e11f648583d90a041d4636758fc8fb065925244a
fiftekhar <iftekhar.f.19@gmail.com>
2022-12-23 23:15:38 -0500

    changed port from 5001 to 5000 to match new backend for app

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -2,7 +2,7 @@ import * as ac from './actions/actionCreators';
 const axios = require('axios');
 
 //PATH (should be where your server is running)
-let path = "http://localhost:5001/api";
+let path = "http://localhost:5000/api";
 
 // THUNKS
 
