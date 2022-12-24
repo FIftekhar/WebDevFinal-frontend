a07cdc62a03a1d6c63d220c600ef4445ea14ca30
fiftekhar <iftekhar.f.19@gmail.com>
2022-12-23 23:24:18 -0500

    added components into app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Router
 import { Switch, Route } from "react-router-dom";
-
 //Components
 import {
 	HomePageContainer,
-	AllTasksContainer,
-	AllEmployeesContainer,
 	EmployeeContainer,
 	TaskContainer,
-	NewTaskContainer
+	AllEmployeesContainer,
+	AllTasksContainer,
+	NewTaskContainer,
+	EditTaskContainer,
+	EditEmployeeContainer,
+	NewEmployeeContainer
 } from './components/containers';
 
-// if you create separate components for adding/editing 
-// a student or instructor, make sure you add routes to those
-// components here
-
 const App = () => {
-	return (
-		<div className="App">
-			<Switch>
-				{/* <Route exact path="/" component={HomePageContainer} />
-				<Route exact path="/instructors" component={AllInstructorsContainer} />
-				<Route exact path="/instructor/:id" component={InstructorContainer} />
-				<Route exact path="/courses" component={AllCoursesContainer} />
-				<Route exact path="/newcourse" component={NewCourseContainer} />
-				<Route exact path="/course/:id" component={CourseContainer} /> */}
-
-				<Route exact path="/" component={AllTasksContainer} /> {/* starts on all tasks view */}
-				<Route exact path="/home" component={HomePageContainer} /> {/* shows normal home page */}
-				<Route exact path="/employees" component={AllEmployeesContainer} /> {/* shows all employees */}
-				<Route exact path="/employee/:id" component={EmployeeContainer} /> {/* shows single employee */}
-				<Route exact path="/tasks" component={AllTasksContainer} /> {/* shows all tasks */}
-				<Route exact path="/task/:id" component={TaskContainer} /> {/* shows single task */}
-				<Route exact path="/newtask" component={NewTaskContainer} />
-			</Switch>        
-		</div>
-	);
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePageContainer} />
+        <Route exact path="/employees" component={AllEmployeesContainer} />
+        <Route exact path="/employee/:id" component={EmployeeContainer} />
+        <Route exact path="/tasks" component={AllTasksContainer} />
+        <Route exact path="/newtask" component={NewTaskContainer} />
+        <Route exact path="/task/:id" component={TaskContainer} />
+        <Route exact path="/edittask/:id" component={EditTaskContainer} />
+        <Route exact path="/editemployee/:id" component={EditEmployeeContainer} />
+        <Route exact path="/newemployee" component={NewEmployeeContainer} />
+      </Switch>        
+    </div>
+  );
 }
 
 export default App;
